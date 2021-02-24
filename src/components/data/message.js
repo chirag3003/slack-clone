7e83375ec1559b43ffa56261810733a8b8ac3a9f
chirag <chiragbhalotia0412@gmail.com>
2021-02-24 11:01:57 +0530

    .

--- src/components/data/message.js
+++ src/components/data/message.js
@@ -13,5 +13,10 @@ export default [
         name:'jonas',
         avatar:'https://www.flaticon.com/premium-icon/icons/svg/3829/3829543.svg',
         message:'I am super pumped to work on this'
+    },
+    {
+        name:'How to look',
+        avatar:'https://www.flaticon.com/premium-icon/icons/svg/3829/3829517.svg',
+        message:'Check this link:  https://slack-clone-chirag.web.app/room'
     }
 ]
